09fb652e570bf72fd1774486ae96391b6610fa49
unknown <rodrigolcontreras1317@gmail.com>
2022-03-10 15:01:37 -0300

    puliendo detalles de grid

--- src/components/Blog/BlogCards/BlogCards.jsx
+++ src/components/Blog/BlogCards/BlogCards.jsx
@@ -1,11 +1,8 @@
 import React from 'react'
 import './BlogCards.css'
+import Time from '../../../assets/icons/time.png'
 
 const BlogCards = () => {
-const ObjectArray = [
-    {category:'CIUDAD',title:'Artículo de puente',description:'Un puente es una construcción que permite salvar un accidente geografico',postDATE:'Hace 2m'},
-    {category:'AVENTURA',title:'Artículo de bosque',description:'Lugar poblado de árboles y arbustos.Área con una importante densidad de',postDATE:'Hace 15m'},
-]
 
   return (
     <>
@@ -15,10 +12,19 @@ const ObjectArray = [
       <div>
      
            <div id='BlogCards_BigCards1stImage'>
-            <p></p>
+            <p id='BlogCards_BigCardsCategory'>CIUDAD</p>
+       
+            <p id='BlogCards_BigCardsTitle'>Artículo de puente</p>
+            <p  id='BlogCards_BigCardsDesc'>Un puente es una construcción que permite salvar un accidente geográfico</p>
+            <p id='BlogCards_BigCardsDate'><img id='BlogCards_BigCardsDateTime' src={Time}/>Hace 2m</p>
            </div>
      
            <div id='BlogCards_BigCards2ndImage'>
+           <p id='BlogCards_BigCardsCategory2'>AVENTURA</p>
+       
+            <p id='BlogCards_BigCardsTitle2'>Artículo de bosque</p>
+            <p  id='BlogCards_BigCardsDesc2'>Lugar poblado de árboles y arbustos.Área con una importante densidad de</p>
+            <p id='BlogCards_BigCardsDate2'><img id='BlogCards_BigCardsDateTime2' src={Time}/>Hace 15m</p>
              
            </div>
      
@@ -27,20 +33,23 @@ const ObjectArray = [
          <div id='BlogCards_SmallCardsContainer'>
            <div id='BlogCards_Cards'>
              <div id='BlogCards_CardsImg1'>
-                <p></p>
              </div>
+             <p className='BlogCards_CardsTitle'>Artículo de elefante</p>
+             <p className='BlogCards_CardsPostDate'><img src={Time}/>Hace 3h</p>
             </div>
 
             <div className='BlogCards_CardsMargin'>
              <div id='BlogCards_CardsImg2'>
-
              </div>
+             <p className='BlogCards_CardsTitle'>Artículo de loro</p>
+             <p className='BlogCards_CardsPostDate'><img src={Time}/>Hace 4h</p>
             </div>
 
             <div className='BlogCards_CardsMargin'>
-             <div id='BlogCards_CardsImg3'>
-             
+             <div id='BlogCards_CardsImg3'>        
              </div>
+             <p className='BlogCards_CardsTitle'>Artículo de camino</p>
+             <p className='BlogCards_CardsPostDate'><img src={Time}/>Hace 8h</p>
             </div>
          </div>
       </div>
